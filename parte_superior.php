45524f80d14435f3408c047df87bc55a201e163d
Nixon <nixonsrf@gmail.com>
2024-06-12 21:43:42 -0600

    Primera Clase

---
+++ parte_superior.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="English">
+
+<head>
+    <meta charset="utf-8"> 
+    <meta name="Proyecto de Tercera 1" content="Pagina de prueba">
+    <title>
+        Tercera 1
+    </title>
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+
+<menu>
+    <div class="menu-izquierda">
+        <a href="./index.php">
+        <img src="./Images/menu.png" alt="">
+
+        </a>
+
+        <p class="texto-menu-izquierda">
+            La telarana de la arana 
+        </p>
+
+
+
+    </div>
+
+
+
+</menu>
